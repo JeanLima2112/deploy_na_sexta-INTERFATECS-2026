b0ab32f94767d1227b59bd682fd9291c19839e59
JeanL <jeanl@example.com>
2026-04-25 17:22:42 -0300

    aula 03 e 04

---
+++ C++_Basics/Aula-03/primeiro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std; // standart
+
+int main(){
+    cout << "Hello World! \n" << endl;  // endl e \n pulam a linha 
+    
+    return  0; // definimos a função como int, por temos que retornar qualquer inteiro ao final
+}
